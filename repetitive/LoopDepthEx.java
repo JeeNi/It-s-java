632ae9681703ad81a7b98d3cbd1249a5543da631
JeeNi <parkej9494@gmail.com>
2019-06-06 01:24:31 +0900

    repetitive 06/06

---
+++ repetitive/LoopDepthEx.java
@@ -0,0 +1,12 @@
+package repetitive;
+
+public class LoopDepthEx{
+    public static void main(String[] args) {
+        
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                System.out.println(i + "" + j);
+            }
+        }
+    }
+}
