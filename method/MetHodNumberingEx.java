ca7e1643bf9b4c5600018c964f65ba659e99def5
JeeNi <parkej9494@gmail.com>
2019-06-07 01:15:47 +0900

    method 06/06

---
+++ method/MetHodNumberingEx.java
@@ -0,0 +1,18 @@
+package method;
+
+public class MetHodNumberingEx {
+
+	public static void numbering() {
+		int i = 0;
+		while (i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		numbering();
+	}
+
+}
---
+++ method/MethodEx1.java
@@ -0,0 +1,40 @@
+package method;
+
+public class MethodEx1 {
+	public static String numbering(int init, int limit) {
+		
+		int i = init;
+		
+		// 만들어지는 숫자를 output이라는 변수에 담기 위해
+		// 변수에 빈 값을 줌
+		
+		String output = "";
+		
+		while (i < limit) {
+			
+			// 숫자를 화면에 출력하는 대신 변수 output에 담음
+			
+			output += i;
+			i++;
+		}
+		
+		// **중요
+		// output에 담긴 문자열을 메소드 외부로 반환하려면
+		// 아래와 같이 return 키워드 뒤에 반환하려는 값을 배치
+		
+		return output;
+	
+	}
+	public static void main(String[] args) {
+		
+		// numbering 메소드에서 리턴한 값이 result 변수에 담김
+		
+		String result = numbering(1, 5);
+		
+		// result 변수의 값을 화면에 출력함
+		
+		System.out.println(result);
+				
+	}
+
+}
