ba030301c42882ed5b2af4faf529ae26f2dd3c3d
JeeNi <parkej9494@gmail.com>
2019-06-13 00:01:13 +0900

    scope modify

--- scope/ScopeGlobalVariable.java
+++ scope/ScopeGlobalVariable.java
@@ -4,11 +4,11 @@ public class ScopeGlobalVariable {
 	static int i; 			// 전역변수
 	
 	static void a() {
-		i = 0;
+		/*int*/i = 0;
 	}
 
 	public static void main(String[] args) {
-		for (i = 0; i < 5; i++) {
+		for (/*int*/ i = 0; i < 5; i++) {			// 주석처럼 int를 넣어주면 01234가 출력됨
 			a();
 			System.out.println(i);
 		}
