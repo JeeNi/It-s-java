d43297a9745ada00f59451ac2581230505a52d75
JeeNi <parkej9494@gmail.com>
2019-06-17 01:19:35 +0900

    inheritance 06/17

--- inheritance/InheritanceEx1.java
+++ inheritance/InheritanceEx1.java
@@ -25,10 +25,10 @@ class Calculator {
 }
 class SubstractionableCalculator extends Calculator {
     public SubstractionableCalculator(int left, int right) {
-        super(left, right);
-    }
- 
-    public void substract() {
+        super(left, right);				// super는 부모 클래스를 의미 // this가 자기 자신을 의미하는 것과 같은 원리
+    }									// super라고 쓰고 괄호가 붙어있으면 부모 클래스의 생성자를 의미
+    									// 하위 클래스의 초기화 코드를 super 보다 먼저 등장시키면 안됨 
+    public void substract() {			// 하위 클래스가 만들어진다는 것은 상위 클래스가 이미 인스턴트화 됐다는 뜻이기 때문에, 상위 클래스의 생성자를 호출한 다음에 나타나야함
         System.out.println(this.left - this.right);
     }
 }
