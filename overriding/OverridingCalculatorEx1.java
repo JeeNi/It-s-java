3a39898625722222a86f9558b2a0f5fb628125d0
JeeNi <parkej9494@gmail.com>
2019-06-18 23:56:13 +0900

    overriding. re

--- overriding/OverridingCalculatorEx1.java
+++ overriding/OverridingCalculatorEx1.java
@@ -1,15 +1,13 @@
 package overriding;
 
+
+
 class Calculator {
     int left, right;
  
     public void setOprands(int left, int right) {
         this.left = left;
         this.right = right;
-    }
- 
-    public void sum() {
-        System.out.println(this.left + this.right);
     }
  
     public int avg() {
