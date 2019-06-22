506fa823af76b6878804274ea96360b45b8268be
JeeNi <parkej9494@gmail.com>
2019-06-22 11:36:21 +0900

    accessmodifier 06/22

---
+++ accessmodifier/AccessEx2.java
@@ -0,0 +1,34 @@
+package accessmodifier;
+
+class Calculator{
+	private int left, right;
+	
+	public void setOperands(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	private int _sum() {
+		return this.left + this.right;
+	}
+	
+	public void sumDecoPlus() {
+		System.out.println("++++" + _sum() + "++++");
+	}
+	
+	public void sumDecoMinus() {
+		System.out.println("----" + _sum() + "----");
+	}
+}
+
+public class AccessEx2 {
+
+	public static void main(String[] args) {
+		Calculator c1 = new Calculator();
+		c1.setOperands(10, 20);
+		c1.sumDecoPlus();
+		c1.sumDecoMinus();
+
+	}
+
+}
